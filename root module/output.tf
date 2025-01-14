06bc81c2f9d8705f828676096a72d163610fe1dd
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-14 16:30:23 +0530

    Update output.tf

--- root module/output.tf
+++ root module/output.tf
@@ -17,3 +17,43 @@ output "eks_cluster_name" {
 output "eks_cluster_sg_id" {
   value = module.vpc.eks_cluster_sg_id 
 }
+
+output "eks_cluster_role_arn" {
+  value = module.iam.eks_cluster_role_arn
+}
+
+output "eks_cluster_role_name" {
+  value = module.iam.eks_cluster_role_name
+}
+
+output "eks_worker_node_role_arn" {
+  value = module.iam.eks_worker_node_role_arn
+}
+
+output "eks_worker_node_role_name" {
+  value = module.iam.eks_worker_node_role_name
+}
+
+output "eks_cluster_policy_attachment_arn" {
+  value = module.iam.eks_cluster_policy_attachment_arn
+}
+
+output "eks_vpc_controller_policy_attachment_arn" {
+  value = module.iam.eks_vpc_controller_policy_attachment_arn
+}
+
+output "eks_worker_node_policy_attachment_arn" {
+  value = module.iam.eks_worker_node_policy_attachment_arn
+}
+
+output "eks_cni_policy_attachment_arn" {
+  value = module.iam.eks_cni_policy_attachment_arn
+}
+
+output "cloudwatch_policy_attachment_arn" {
+  value = module.iam.cloudwatch_policy_attachment_arn
+}
+
+output "eks_ecr_read_only_policy_attachment_arn" {
+  value = module.iam.eks_ecr_read_only_policy_attachment_arn
+}
