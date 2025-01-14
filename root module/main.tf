ad3a56b33860d215c8a17c58b00f5bbe0313734d
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-14 12:50:54 +0530

    Update main.tf

--- root module/main.tf
+++ root module/main.tf
@@ -5,6 +5,7 @@ module "vpc" {
   public_subnet_cidrs = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
   cluster_name       = var.cluster_name
+  cluster_sg_id    = module.vpc.eks_cluster_sg_id
   tags               = var.tags
 }
 
