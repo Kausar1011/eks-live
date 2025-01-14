7d3c416b09eefdc7b24b490e61948937f649f8c6
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-14 11:03:25 +0530

    Update variable.tf

--- module/eks/variable.tf
+++ module/eks/variable.tf
@@ -1,39 +1,38 @@
-
 variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
   default     = "my-eks-cluster"
 }
-  
-variable "vpc_id" {
-  description = "EKS cluster vpc id"
-  type        = string
-  default     = "aws_vpc.main.id"
-}
 
+variable "vpc_id" {
+  description = "VPC ID for the EKS cluster"
+  type        = string
+  default     = "vpc-03287220535c90cd1" # Updated VPC ID
+}
 
 variable "cluster_role_arn" {
   description = "IAM Role ARN for the EKS cluster"
   type        = string
-  default     = "arn:aws:iam::123456789012:role/EKS-ClusterRole" # Replace with your actual ARN
+  default     = "arn:aws:iam::423623842683:role/cluster_role"
+
 }
 
 variable "node_role_arn" {
   description = "IAM Role ARN for the EKS worker nodes"
   type        = string
-  default     = "arn:aws:iam::123456789012:role/EKS-NodeRole" # Replace with your actual ARN
+  default     =  "arn:aws:iam::423623842683:role/worker-node"
+                          
 }
 
 variable "subnet_ids" {
   description = "List of subnet IDs where the EKS cluster and nodes will be deployed"
   type        = list(string)
-  default     = ["subnet-0a1b2c3d4e5f6g7h8", "subnet-1a2b3c4d5e6f7g8h9"] # Replace with your subnet IDs
-}
-
-variable "cluster_sg_id" {
-  description = "Security group ID for the EKS cluster"
-  type        = string
-  default     = "sg-0a1b2c3d4e5f6g7h8" # Replace with your security group ID
+  default     = [
+    "subnet-000d2c85eb4c3802b", # Private Subnet 1
+    "subnet-01f4f392c50a9be67", # Private Subnet 2
+    "subnet-016953a10fe799255", # Public Subnet 1
+    "subnet-0e01483cbe21b0060"  # Public Subnet 2
+  ]
 }
 
 variable "desired_size" {
@@ -45,16 +44,11 @@ variable "desired_size" {
 variable "max_size" {
   description = "Maximum number of worker nodes in the EKS node group"
   type        = number
-  default     = 5
+  default     = 4
 }
 
 variable "min_size" {
   description = "Minimum number of worker nodes in the EKS node group"
   type        = number
   default     = 1
-}
-
-variable "tags" {
-  description = "Tags to apply to all resources"
-  type        = map(string)
 }
