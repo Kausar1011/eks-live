b461dc6d0c5b48cd642696d6b72505863f3c2a3d
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-14 18:27:21 +0530

    Update iam.tf

--- module/iam/iam.tf
+++ module/iam/iam.tf
@@ -17,17 +17,22 @@ resource "aws_iam_role" "cluster_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
-  role       = aws_iam_role.eks_cluster_role.name
+  role       = aws_iam_role.cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_vpc_controller_policy_attachment" {
-  role       = aws_iam_role.eks_cluster_role.name
+  role       = aws_iam_role.cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 }
 
+resource "aws_iam_role_policy_attachment" "eks_ecr_read_only" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 # IAM Role for Worker Nodes
-resource "aws_iam_role" "worker-node" {
+resource "aws_iam_role" "worker_node" {
   name = "${var.cluster_name}-worker-node"
 
   assume_role_policy = jsonencode({
@@ -45,23 +50,16 @@ resource "aws_iam_role" "worker-node" {
 }
 
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy_attachment" {
-  role       = aws_iam_role.eks_node_role.name
+  role       = aws_iam_role.worker_node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cni_policy_attachment" {
-  role       = aws_iam_role.eks_node_role.name
+  role       = aws_iam_role.worker_node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "cloudwatch_policy_attachment" {
-  role       = aws_iam_role.eks_node_role.name
+  role       = aws_iam_role.worker_node.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
-
-resource "aws_iam_role_policy_attachment" "eks_ecr_read_only" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-
