411d9527e2714f34f3e42b1a497bd1a332c4bb28
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-14 10:59:50 +0530

    Update output.tf

--- module/eks/output.tf
+++ module/eks/output.tf
@@ -7,3 +7,8 @@ output "node_group_name" {
   description = "The name of the EKS node group"
   value       = aws_eks_node_group.eks_node_group.node_group_name
 }
+
+output "security_group_id" {
+  description = "The ID of the security group created for the EKS cluster"
+  value       = aws_security_group.eks_sg.id
+}
