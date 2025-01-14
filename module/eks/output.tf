b1b519b857e401b4b7575b84c3bf8dacebef673a
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-14 11:45:11 +0530

    Update output.tf

--- module/eks/output.tf
+++ module/eks/output.tf
@@ -12,3 +12,7 @@ output "security_group_id" {
   description = "The ID of the security group created for the EKS cluster"
   value       = aws_security_group.eks_sg.id
 }
+
+output "eks_cluster_sg_id" {
+  value = aws_security_group.eks_cluster_sg.id
+}
