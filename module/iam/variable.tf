de862cf68a779597e0cb6f50637c306f9e83e2ac
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-14 10:55:33 +0530

    Update variable.tf

--- module/iam/variable.tf
+++ module/iam/variable.tf
@@ -1,1 +1,20 @@
 
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to the IAM roles"
+  type        = map(string)
+  default     = {
+    Environment = "dev"
+    Owner       = "admin"
+  }
+}
