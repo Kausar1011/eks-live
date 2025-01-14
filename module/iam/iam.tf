9d9b2ad42b077b26275f8a7c5a16640f70c3a82e
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-14 17:59:58 +0530

    Update iam.tf

--- module/iam/iam.tf
+++ module/iam/iam.tf
@@ -63,3 +63,14 @@ resource "aws_iam_role_policy_attachment" "eks_ecr_read_only" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+# Output ARNs
+output "cluster_role_arn" {
+  description = "ARN of the IAM role for the EKS cluster"
+  value       = aws_iam_role.eks_cluster_role.arn
+}
+
+output "node_role_arn" {
+  description = "ARN of the IAM role for the EKS worker nodes"
+  value       = aws_iam_role.eks_node_role.arn
+}
