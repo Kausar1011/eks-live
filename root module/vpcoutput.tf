5808ede57d891d1ae005e68443b9c7485f8ea1fe
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-15 10:52:19 +0530

    Update vpcoutput.tf

--- root module/vpcoutput.tf
+++ root module/vpcoutput.tf
@@ -1,1 +1,22 @@
+# vpcoutput.tf
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "subnet_ids" {
+  description = "The IDs of the subnets in the VPC"
+  value       = module.vpc.subnet_ids
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "vpc_azs" {
+  description = "The availability zones for the VPC"
+  value       = module.vpc.vpc_azs
+}
 
