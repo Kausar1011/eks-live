c3fad34190d26139af039bc3e80e27e75951a563
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-15 11:08:13 +0530

    Update eksoutput.tf

--- root module/eksoutput.tf
+++ root module/eksoutput.tf
@@ -2,7 +2,7 @@
 
 output "eks_cluster_role_arn" {
   description = "The ARN of the EKS Cluster IAM Role"
-  value       = module.iam.cluster_role_arn
+  value       = module.iam.aws_cluster_role_arn
 }
 
 output "eks_worker_node_role_arn" {
