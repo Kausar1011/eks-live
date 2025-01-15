4e71463837849cbe9f55fa9b9ed72851a6a4b9f8
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-15 12:06:20 +0530

    Update vpcoutput.tf

--- root module/vpcoutput.tf
+++ root module/vpcoutput.tf
@@ -1,6 +1,26 @@
-output "subnet_ids" {
-  description = "The IDs of the subnets"
-  value       = module.vpc.subnet_ids
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.aws_vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = module.vpc.private_subnet_ids
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = module.vpc.internet_gateway_id
+}
+
+output "nat_gateway_id" {
+  description = "The ID of the NAT Gateway"
+  value       = module.vpc.nat_gateway_id
 }
 
 output "vpc_cidr_block" {
@@ -11,26 +31,4 @@ output "vpc_cidr_block" {
 output "vpc_azs" {
   description = "The availability zones for the VPC"
   value       = module.vpc.vpc_azs
-}
-
-output "eks_cluster_sg_id" {
-  description = "The security group ID for the EKS cluster"
-  value       = module.vpc.eks_sg_id
-}
-
-output "subnet_ids" {
-  description = "The IDs of the subnets"
-  value       = module.vpc.subnet_ids
-}
-
-# Output the Public Subnet IDs from the VPC module
-output "subnet_ids" {
-  description = "The IDs of the public subnets"
-  value       = module.vpc.public_subnet_ids
-}
-
-# Output the Private Subnet IDs from the VPC module (if needed)
-output "private_subnet_ids" {
-  description = "The IDs of the private subnets"
-  value       = module.vpc.private_subnet_ids
 }
