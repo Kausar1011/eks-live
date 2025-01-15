c025259e02a61f2e1fbc427cd237e0fd9e1a7069
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-01-15 11:04:54 +0530

    Update output.tf

--- module/iam/output.tf
+++ module/iam/output.tf
@@ -1,9 +1,9 @@
 output "cluster_role_arn" {
   description = "ARN of the IAM role for the EKS cluster"
-  value       = var.cluster_role_arn
+  value       = aws_iam_role.cluster_role.arn
 }
 
 output "node_role_arn" {
   description = "ARN of the IAM role for the EKS worker nodes"
-  value       = var.node_role_arn
+  value       = aws_iam_role.worker_node.arn
 }
